f907f034bf815f636d5690f64159cc5c2298c62c
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-02-06 23:51:43 +0200

    Update arabic.js

--- arabic.js
+++ arabic.js
@@ -1,34 +1,48 @@
 const ARABIC_DATA = [
     {
-        name: "أ. محمد صلاح",
-        img: "https://i.postimg.cc/ZRJy8gDR/FB-IMG-1770382315133.jpg",
-        chapters: [
+        "name": "أ.محمد صلاح",
+        "img": "https://files.coursatk.online/images/questions/abc2fcc42883dd1a9e93021922da73dc92ff4b40ec76.jpg",
+        "chapters": [
             {
-                chapter_name: "الشهر الأول",
-                lectures: [
+                "chapter_name": "الشهر الأول",
+                "lectures": [
                     {
-                        lecture_name: "المحاضرة الأولى",
-                        videos: [
-                            { video_name: "النحو - المبتدأ والخبر", links: [{ url: "https://river-5-545.rtbcdn.ru/hls-vod/lMLIGlXXPat9W2owCUKuWw/1770646511/3320/0x5000c500e9daa4c6/c24ad275e16e460e8f576a81bf0d7ba5.mp4.m3u8" }] },
-                            { video_name: "تدريبات النحو", links: [{ url: "https://river-4-408.rtbcdn.ru/hls-vod/qdH6WZRQ3JFTORURp6p3XQ/1770495006/3300/0x5000c500e9ebeda0/53cfd16b80ba4906a971b900342fda56.mp4.m3u8" }] },
-                            { video_name: "الأدب - الكلاسيكية", links: [{ url: "https://river-5-554.rtbcdn.ru/hls-vod/1ZxXO6yUAYubNz9iuqRsZg/1770665380/3358/0x5000c500fa77c05c/724a5005e0794f1db5e6b027aa11372b.mp4.m3u8" }] }
+                        "lecture_name": "المحاضرة الأولى",
+                        "videos": [
+                            {
+                                "video_name": "النحو - المبتدأ والخبر",
+                                "link": "https://river-5-545.rtbcdn.ru/hls-vod/lMLIGlXXPat9W2owCUKuWw/1770646511/3320/0x5000c500e9daa4c6/c24ad275e16e460e8f576a81bf0d7ba5.mp4.m3u8"
+                            },
+                            {
+                                "video_name": "تدريبات الحصة في النحو",
+                                "link": "https://river-4-408.rtbcdn.ru/hls-vod/qdH6WZRQ3JFTORURp6p3XQ/1770495006/3300/0x5000c500e9ebeda0/53cfd16b80ba4906a971b900342fda56.mp4.m3u8"
+                            },
+                            {
+                                "video_name": "الأدب - المدرسة الكلاسيكية",
+                                "link": "https://river-5-554.rtbcdn.ru/hls-vod/1ZxXO6yUAYubNz9iuqRsZg/1770665380/3358/0x5000c500fa77c05c/724a5005e0794f1db5e6b027aa11372b.mp4.m3u8"
+                            }
                         ]
                     }
                 ]
             },
             {
-                chapter_name: "معسكر 4-1",
-                lectures: [
+                "chapter_name": "معسكر 4 - 1",
+                "lectures": [
                     {
-                        lecture_name: "المهمة الأولى",
-                        videos: [
-                            { video_name: "مقدمة المهمة", links: [{ url: "https://river-4-404.rtbcdn.ru/hls-vod/EHn5i5DByob8O9pOLkRvmg/1770645966/3310/0x5000c500e9b6282b/f6c8e8d620f3450495303e888fe1ede1.mp4.m3u8" }] },
-                            { video_name: "شرح النحو", links: [{ url: "https://river-4-429.rtbcdn.ru/hls-vod/pY6nkzrcG4lHcv1P_FKB1w/1770639813/3322/0x5000c500e9ebd5af/7ed17944068f431bbc6a44ed612999bd.mp4.m3u8" }] }
+                        "lecture_name": "المهمة الأولى",
+                        "videos": [
+                            {
+                                "video_name": "مقدمة المهمة الأولي 4-1",
+                                "link": "https://river-4-404.rtbcdn.ru/hls-vod/EHn5i5DByob8O9pOLkRvmg/1770645966/3310/0x5000c500e9b6282b/f6c8e8d620f3450495303e888fe1ede1.mp4.m3u8"
+                            },
+                            {
+                                "video_name": "شرح الـنحو_ المهمة الأولي 4-1",
+                                "link": "https://river-4-429.rtbcdn.ru/hls-vod/pY6nkzrcG4lHcv1P_FKB1w/1770639813/3322/0x5000c500e9ebd5af/7ed17944068f431bbc6a44ed612999bd.mp4.m3u8"
+                            }
                         ]
                     }
                 ]
             }
         ]
-    },
-    { name: "أ. رضا الفاروق", img: "https://i.postimg.cc/c4Pg965p/1723904748-y-CL9UA.png", chapters: [] }
+    }
 ];
