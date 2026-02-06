c88688418050eda76c49200c1767dd9bfc9bb98a
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-02-06 23:49:25 +0200

    Create langs.js

---
+++ langs.js
@@ -0,0 +1,6 @@
+const LANG2_DATA = [
+    { name: "فرنساوي (أنس السعيد)", img: "https://i.postimg.cc/sDdy3JyV/lhctmi-Qt-ANEg0PDcec-CE1QTCqr-Kza-Eq-GZ6s-Zt-MJ4.jpg", folders: [{name:"المنهج", videos:[{title:"جزء 1", link:"https://www.youtube.com/watch?v=3PC1fxpp-Xk"}]}] },
+    { name: "ألماني (هير ياسين)", img: "https://i.postimg.cc/MHkkKzCd/hqdefault-(1).jpg", folders: [{name:"تأسيس", videos:[{title:"المحاضرة 1", link:"https://www.youtube.com/watch?v=JOLc4Jc9yFU"}]}] },
+    { name: "إيطالي (سنيور مدحت)", img: "https://i.postimg.cc/0NQ1VWGy/hqdefault.jpg", folders: [{name:"مراجعة", videos:[{title:"شامل", link:"https://www.youtube.com/watch?v=T6hpZH5AMnk"}]}] },
+    { name: "إسباني (سنيور ماجد)", img: "https://i.postimg.cc/L8jpqg95/250px-Flag-of-Spain-svg.png", folders: [{name:"مراجعة", videos:[{title:"شرح كامل", link:"https://www.youtube.com/watch?v=K9ZOydc1cqs"}]}] }
+];
